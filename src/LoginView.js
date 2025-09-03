b44779a568c3f39a2af34c2f6a107d7f18144bef
Luke Simmons <lucasjsimmons@yahoo.com>
2025-09-03 16:17:06 -0400

    Revert LoginView to working (but ugly) state

--- src/LoginView.js
+++ src/LoginView.js
@@ -4,28 +4,27 @@ class LoginView {
   }
 
   draw(container) {
-    const form = this.render_form()
-    container.innerHTML = form
+    container.innerHTML = ''
+    const form = document.createElement('form')
+    form.innerHTML = this.form_inputs()
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const data = new FormData(form)
+      this.startGame(data.get('name'), data.get('num_of_bots'))
+    })
+    container.append(form)
   }
 
-  render_form() {
+  form_inputs() {
     return `
-    <form class="form" onsubmit="${this.submitForm}">
-      <div class="form-group">
-        <label for="name">Name</label>
-        <input class="form-control" type="text" name="name" id="name" required />
-      </div>
-      <div class="form-group">
-        <label for="num_of_bots">Number of bots</label>
-        <input class="form-control" type="number" name="num_of_bots" id="num_of_bots" value=1 />
-      </div>
-      <input class="btn btn--primary" type="submit" />
-    </form>`
-  }
-
-  submitForm(event) {
-    event.preventDefault()
-    const data = new FormData(event.target)
-    this.startGame(data.get('name'), data.get('num_of_bots'))
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input class="form-control" type="text" name="name" id="name" required />
+    </div>
+    <div class="form-group">
+      <label for="num_of_bots">Number of bots</label>
+      <input class="form-control" type="number" name="num_of_bots" id="num_of_bots" value=1 />
+    </div>
+    <input class="btn btn--primary" type="submit" />`
   }
 }
